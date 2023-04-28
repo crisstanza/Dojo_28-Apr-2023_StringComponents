0e7aca1ffd5066e3b296990530f894fd847d8194
Rodrigo Andrade <rodrigo.andrade@wunderkind.co>
2023-04-28 11:06:33 -0300

    Recent changes...

--- js/TextBoxBuilder.js
+++ js/TextBoxBuilder.js
@@ -1,13 +1,27 @@
 class TextBoxBuilder {
+	#builder;
 
-	constructor(settings) {
-		console.log('TextBoxBuilder.constructor(' + JSON.stringify(settings) + ')');
+	// constructor(settings) {
+	// 	console.log('TextBoxBuilder.constructor(' + JSON.stringify(settings) + ')');
+	// }
+
+	constructor() {
+		this.#builder = new NewLineStringBuilder();
 	}
 
 	append(value) {
+		this.#builder.append(value);
 	}
 
 	toString() {
+		largestLine;
+		this.#builder.lines.forEach(line => {
+			if (line.length > largestLine.length) {
+				largestLine = line;
+			}
+		});
+
+
 	}
 
 }
