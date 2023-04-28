39bf99f8ba42cb59b28da3dc3104278f634375a9
Gustavo Ornaghi Antunes <gustavo.ornaghiantunes@windriver.com>
2023-04-28 11:21:39 -0300

    Recent changes...

--- js/TextBoxBuilder.js
+++ js/TextBoxBuilder.js
@@ -14,14 +14,19 @@ class TextBoxBuilder {
 	}
 
 	toString() {
-		largestLine;
+		let largestLine = '';
 		this.#builder.lines.forEach(line => {
 			if (line.length > largestLine.length) {
 				largestLine = line;
 			}
 		});
-
-
+		let string = this.#builder.lines.map(line => {
+			let emptyChar = largestLine.length - line.length;
+			for (let index = 0; index < emptyChar; index++) {
+				line += ' ';
+			}
+			return '='+line+'=';
+		});
 	}
 
 }
